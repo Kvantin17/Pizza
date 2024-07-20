71b31f5e264d1e8f0e8ad2fa9eb3e6c576d94681
Kvantin17 <tvantin17@gmail.com>
2024-07-20 14:45:32 +0300

    Menu loader

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line react/prop-types
+const MenuItem = ({ pizza }) => {
+  return (
+    <li>
+      <div>{pizza.name}</div>
+    </li>
+  );
+};
+
+export default MenuItem;
