abfad2343a3319ea300437850951c6a35bd2912a
Kvantin17 <tvantin17@gmail.com>
2024-07-20 15:44:05 +0300

    Handling Errors With Error Elements

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import LoadingComponent from "./LoadingComponent";
 
 const AppLayout = () => {
-  // Этот хук дает инфу о том загружается ли страница или нет
+  // Этот хук дает инфу о том загружается ли страница или нет || не путать с useNavigate
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
 
