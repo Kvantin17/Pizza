44b121b7f79c50757067f9a814de9a6aa6453b8a
Kvantin17 <tvantin17@gmail.com>
2024-07-20 23:21:43 +0300

    order and search order

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -2,8 +2,16 @@ const API_URL = "https://react-fast-pizza-api.onrender.com/api";
 
 export async function getMenu() {
   const res = await fetch(API_URL + "/menu");
-  if (!res.ok) throw new Error("Failed getting menu");
+  if (!res.ok) throw Error("Failed getting menu");
   const { data } = await res.json();
 
+  return data;
+}
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+  if (!res.ok) throw Error(`Don't find order #${id}`);
+
+  const { data } = await res.json();
   return data;
 }
