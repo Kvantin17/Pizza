cd014bfe1f556208fa9ffc2813867bcd6168e548
Kvantin17 <tvantin17@gmail.com>
2024-07-21 22:49:58 +0300

    fix create user

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
-import Button from '../../ui/Button';
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button";
 
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -10,21 +10,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,7 +34,7 @@ const fakeCart = [
 function CreateOrder() {
   // как в AppLayout только тут action а не Loader
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
 
   //Хук чтоб вернуть данные из action (лучше использовать чтоб вернуть ошибку)
   const formErrors = useActionData();
@@ -77,7 +77,7 @@ function CreateOrder() {
           собрал значение */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+            {isSubmitting ? "Placing order..." : "Order now"}
           </Button>
         </div>
       </Form>
@@ -85,6 +85,7 @@ function CreateOrder() {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
@@ -92,12 +93,12 @@ export const action = async ({ request }) => {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === "on",
   };
 
   const errors = {};
   if (!isValidPhone(order.phone))
-    errors.phone = 'Please give us your correct phone number.';
+    errors.phone = "Please give us your correct phone number.";
 
   if (Object.keys(errors).length > 0) return errors;
 
