a79040e357a7ae757b09998583e338f9b08193e2
Kvantin17 <tvantin17@gmail.com>
2024-07-22 21:25:59 +0300

    Update Order

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,19 @@
-import { Outlet, useNavigation } from 'react-router-dom';
-import CartOverview from '../features/cart/CartOvervie';
-import Header from './Header';
-import LoadingComponent from './LoadingComponent';
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOvervie";
+import Header from "./Header";
+import LoadingComponent from "./LoadingComponent";
 
 const AppLayout = () => {
   // Этот хук дает инфу о том загружается ли страница или нет || не путать с useNavigate
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <LoadingComponent />}
 
       <Header />
-      <div>
+      <div className="overflow-auto">
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
